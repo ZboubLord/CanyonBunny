95de9c2bb0a59283ab5bf0a7da0d2bea239dbcc6
Sargis Sargsyan <sargsyan.sargis.89@gmail.com>
2014-08-11 18:05:09 +0400

    chapter 6, nachalo

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Feather.java
@@ -0,0 +1,45 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+/**
+ * Created by sargis on 8/5/14.
+ */
+public class Feather extends AbstractGameObject {
+    public boolean collected;
+    private TextureRegion regFeather;
+
+    public Feather() {
+        init();
+    }
+
+    private void init() {
+        dimension.set(0.5f, 0.5f);
+        regFeather = Assets.instance.feather.feather;
+        // Set bounding box for collision detection
+        bounds.set(0, 0, dimension.x, dimension.y);
+        collected = false;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        if (collected) return;
+        TextureRegion reg = null;
+        reg = regFeather;
+        batch.draw(reg.getTexture(),
+                position.x, position.y,
+                origin.x, origin.y,
+                dimension.x, dimension.y,
+                scale.x, scale.y,
+                rotation,
+                reg.getRegionX(), reg.getRegionY(),
+                reg.getRegionWidth(), reg.getRegionHeight(),
+                false, false);
+    }
+
+    public int getScore() {
+        return 250;
+    }
+}
